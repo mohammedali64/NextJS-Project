3b7baf2046d86cd2b43313fddaeecf5d00d874c0
Mohammed Ali <mohammedali88870@gmail.com>
2025-08-04 11:05:10 +0530

    added the static files like image

--- new_project/src/app/products/[id]/page.js
+++ new_project/src/app/products/[id]/page.js
@@ -1,6 +1,9 @@
+import Image from "next/image";
+
 export default async function ProductDetailPage({ params }) {
   const { id } = params;
 
+  // Fetch product details
   const res = await fetch(`https://dummyjson.com/products/${id}`, {
     next: { revalidate: 60 },
   });
@@ -11,11 +14,20 @@ export default async function ProductDetailPage({ params }) {
       border: "2px solid #000",
       padding: "20px",
       width: "fit-content",
-      margin: "20px auto"
+      margin: "20px auto",
+      textAlign: "center"
     }}>
       <h2>{product.title}</h2>
       <p>{product.description}</p>
       <p><strong>Price:</strong> ${product.price}</p>
+
+      <Image
+        src="/one-piece.avif" 
+        alt="Product placeholder"
+        width={300}
+        height={300}
+        style={{ marginTop: "20px" }}
+      />
     </div>
   );
 }
